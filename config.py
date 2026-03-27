974d9aaa263f8ac818a19765f0f95c7f4ed5c027
Ludwig Cardoso <ingenriquecardosoalfonso@gmail.com>
2026-03-27 12:14:31 -0600

    Publish

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
+import os
 from urllib.parse import quote_plus
 
 class Config:
-    SECRET_KEY = "super_secret_key"
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'super_secret_key')
 
-    _pwd = quote_plus("Ust@1411*****")  # ← solo encodea la contraseña
-    
-    SQLALCHEMY_DATABASE_URI = (
-        f"mssql+pyodbc://ludwig:{_pwd}@suumph.database.windows.net:1433/NoLine"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or (
+        "mssql+pyodbc://ludwig:{}@suumph.database.windows.net:1433/NoLine"
         "?driver=ODBC+Driver+18+for+SQL+Server"
         "&Encrypt=yes"
         "&TrustServerCertificate=no"
         "&Connection+Timeout=30"
-    )
+    ).format(quote_plus("Ust@1411*****"))
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
